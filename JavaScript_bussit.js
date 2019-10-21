465c05db52172d7ad1856e0b067703d26704ad5f
Ria Ahlström <ria.ahlstrom@gmail.com>
2019-10-21 10:04:35 +0300

    bussit lisätty hakuun

--- JavaScript_bussit.js
+++ JavaScript_bussit.js
@@ -49,6 +49,8 @@ var a = setInterval(metrot, 120000);
 
 function metrot() { 
     document.getElementById("metrolista").innerHTML = "";
+    document.getElementById("bussilista").innerHTML = "";
+
 
     fetch(url, opts)
     .then(res => res.json(res))
@@ -60,61 +62,66 @@ function metrot() {
         console.log(stops);
         for (var i = 0; i < stops.length; i++) {
             //let linjat = stops[i].stoptimesWithoutPatterns;
-            let id = stops[i].gtfsId;            
-            if (id === "HSL:2314602" || id === "HSL:2314601") {
-                console.log("Metro ID " + id);
-                let next = stops[i].stoptimesWithoutPatterns;
-                for (var i = 0; i < next.length; i++) {
-                    console.log(next[i].headsign)
-                    var unixtime = parseInt(next[i].scheduledArrival) + parseInt(next[i].serviceDay);
-                    var date = new Date(unixtime * 1000);
-                    var hours = date.getHours();
-                    var minutes = "0" + date.getMinutes();
-                    var formattedTime = hours + ':' + minutes.substr(-2);
-                    console.log(formattedTime);
-                    var nyt = new Date();
-                    console.log(nyt);
-                    var minutes = diff_minutes(date, nyt);
-                    console.log(minutes + " min");
+            let id = stops[i].gtfsId;             
+            switch (id) {
+                case 'HSL:2314601':
+                    console.log("Metro ID " + id);
+                    let next = stops[i].stoptimesWithoutPatterns;
+                    for (var x = 0; x < next.length; x++) {
+                        console.log(next[i].headsign)
+                        var unixtime = parseInt(next[x].scheduledArrival) + parseInt(next[x].serviceDay);
+                        var date = new Date(unixtime * 1000);
+                        var hours = date.getHours();
+                        var minutes = "0" + date.getMinutes();
+                        var formattedTime = hours + ':' + minutes.substr(-2);
+                        console.log(formattedTime);
+                        var nyt = new Date();
+                        console.log(nyt);
+                        var minutes = diff_minutes(date, nyt);
+                        console.log(minutes + " min");
+                        document.getElementById("metrolista").innerHTML +=
+                            "<tr>"
+                            + "<td>" + formattedTime + "</td>"
+                            + "<td>" + minutes + " min" + "</td>"
+                            + "</tr>";
+                    }
+                    break;
+                case 'HSL:2314210':
+                    console.log("B1" + id);
+                    let d1 = stops[i].patterns;
+                    for (var a = 0; a < d1.length; a++) {
+                        console.log(d1[a].route.shortName);
+                    }
+                    let d2 = stops[i].stoptimesWithoutPatterns;
+                    for (var b = 0; b < d2.length; b++) {
+                        console.log("laituri:" + d2[b].stop.platformCode + ":" + d2[b].trip.routeShortName);
+                        var unixtime = parseInt(d2[b].scheduledArrival) + parseInt(d2[b].serviceDay);
+                        var date = new Date(unixtime * 1000);
+                        var hours = date.getHours();
+                        var minutes = "0" + date.getMinutes();
+                        var formattedTime = hours + ':' + minutes.substr(-2);
+                        console.log(formattedTime);
+                        var nyt = new Date();
+                        console.log(nyt);
+                        var minutes = diff_minutes(date, nyt);
+                        console.log(minutes + " min");
+                        document.getElementById("bussilista").innerHTML +=
+                            "<tr>"
+                        + "<td>" + "laituri:" + d2[b].stop.platformCode + " linja: " + d2[b].trip.routeShortName + "</td>"
+                            + "<td>" + formattedTime + "</td>"
+                            + "<td>" + minutes + " min" + "</td>"
+                            + "</tr>";
+                    }
 
-                    document.getElementById("metrolista").innerHTML +=
-                        "<tr>"
-                        + "<td>" + formattedTime + "</td>"
-                        + "<td>" + minutes + " min" + "</td>"
-                        + "</tr>";
-                }
-            }
-            else {
-                console.log("pysäkin ID " + id);
-            }
+                    break;
 
-            //console.log(linjat);
+                default:
+                    console.log("ID " + id);
+                    break;
+            }       
+                        //console.log(linjat);
         }
-        //let next = json.data.stop.stoptimesWithoutPatterns;
-        //for (var i = 0; i < next.length; i++) {
-        //    console.log(next[i].headsign)          
-        //    var unixtime = parseInt(next[i].scheduledArrival) + parseInt(next[i].serviceDay);
-        //    var date = new Date(unixtime * 1000);
-        //    var hours = date.getHours();
-        //    var minutes = "0" + date.getMinutes();
-        //    var formattedTime = hours + ':' + minutes.substr(-2);
-        //    console.log(formattedTime);
-        //    var nyt = new Date();
-        //    console.log(nyt);
-        //    // vertaa aikoja tässä välissä            
-        //    var minutes = diff_minutes(date, nyt);
-        //    console.log( minutes + " min");
-        //    // vertaus loppuu
 
-        //    //aja tavarat HTML:ään
-        //    document.getElementById("metrolista").innerHTML +=
-        //    "<tr>"
-        //        + "<td>" + formattedTime + "</td>"
-        //        + "<td>" + minutes + " min" + "</td>"
-        //        + "</tr>";
-        //    //lopeta ajaminen
-
-        //}
     })
         .catch(err => console.dir(err));
 }
